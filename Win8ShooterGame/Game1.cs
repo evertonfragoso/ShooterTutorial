6d6020e450c7a7e616360305a672b81c624ee4f2
Everton Fragoso <everton.lfragoso@gmail.com>
2019-08-09 19:12:31 +1200

    First Commit

---
+++ Win8ShooterGame/Game1.cs
@@ -0,0 +1,220 @@
+﻿// using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Shooter;
+
+namespace Win8ShooterGame.Windows
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        private readonly GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        // Represents the player
+        private Player player;
+
+        // Keyboard states used to determine key presses
+        private KeyboardState currentKeyboardState;
+        private KeyboardState previousKeyboardState;
+
+        // Gamepad states used to determine button presses
+        private GamePadState currentGamePadState;
+        private GamePadState previousGamePadState;
+
+        // Mouse states used to track Mouse button press
+        private MouseState currentMouseState;
+        private MouseState previousMouseState;
+
+        // A movement speed for the player
+        private const float playerMoveSpeed = 8;
+
+        public Game1()
+        {
+            IsMouseVisible = true;
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            // Initialize the player class
+            player = new Player();
+
+            // Enable the FreeDrag gesture
+            TouchPanel.EnabledGestures = GestureType.FreeDrag;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+
+            // Load the player resources
+
+            Rectangle titleSafeArea = GraphicsDevice.Viewport.TitleSafeArea;
+            Vector2 playerPosition = new Vector2(titleSafeArea.X, titleSafeArea.Y + titleSafeArea.Height / 2);
+
+            // Static Player - BEGIN ---
+            //Texture2D playerTexture = Content.Load<Texture2D>("Graphics\\player");
+            //player.Initialize(playerTexture, playerPosition);
+            // Static Player - END ---
+
+            // Animated Player - BEGIN ---
+            Animation playerAnimation = new Animation();
+            Texture2D playerTexture = Content.Load<Texture2D>("Graphics\\shipAnimation");
+            playerAnimation.Initialize(playerTexture, playerPosition, 115, 69, 8, 30, Color.White, 1f, true);
+
+            player.Initialize(playerAnimation, playerPosition);
+            // Animated Player - END ---
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            // Save the previous state of the keyboard and game pad so we can determine single key/button presses
+            previousGamePadState = currentGamePadState;
+            previousKeyboardState = currentKeyboardState;
+
+            // Read the current state of the keyboard and gamepad and store it
+            currentKeyboardState = Keyboard.GetState();
+            currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+            // Get Mouse states
+            previousMouseState = currentMouseState;
+            currentMouseState = Mouse.GetState();
+
+            // Update the player
+            UpdatePlayer(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        private void UpdatePlayer(GameTime gameTime)
+        {
+            player.Update(gameTime);    // Animated player
+            //player.Update();          // Static Player
+
+            // Windows 8 Touch Gestures for MonoGame
+            while (TouchPanel.IsGestureAvailable)
+            {
+                GestureSample gesture = TouchPanel.ReadGesture();
+                if (gesture.GestureType == GestureType.FreeDrag)
+                    player.Position += gesture.Delta;
+            }
+
+            // Get Mouse State then Capture the Button type and Respond Button Press
+            Vector2 mousePosition = new Vector2(currentMouseState.X, currentMouseState.Y);
+
+            if (currentMouseState.LeftButton == ButtonState.Pressed)
+            {
+                Vector2 posDelta = mousePosition - player.Position;
+                posDelta.Normalize();
+                posDelta = posDelta * playerMoveSpeed;
+                player.Position = player.Position + posDelta;
+            }
+
+            // Get Thumbstick Controls
+            player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+            player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+            // Use the Keyboard / Dpad
+            if (IsLeftKey()) player.Position.X -= playerMoveSpeed;
+            if (IsRightKey()) player.Position.X += playerMoveSpeed;
+            if (IsUpKey()) player.Position.Y -= playerMoveSpeed;
+            if (IsDownKey()) player.Position.Y += playerMoveSpeed;
+
+            // Make sure that the player does not go out of bounds
+            player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+            player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+        }
+
+        private bool IsUpKey()
+        {
+            return (currentKeyboardState.IsKeyDown(Keys.Up) ||
+                    currentKeyboardState.IsKeyDown(Keys.W) ||
+                    ButtonState.Pressed == currentGamePadState.DPad.Up);
+        }
+
+        private bool IsDownKey()
+        {
+            return (currentKeyboardState.IsKeyDown(Keys.Down) ||
+                    currentKeyboardState.IsKeyDown(Keys.S) ||
+                    ButtonState.Pressed == currentGamePadState.DPad.Down);
+        }
+
+        private bool IsLeftKey()
+        {
+            return (currentKeyboardState.IsKeyDown(Keys.Left) ||
+                    currentKeyboardState.IsKeyDown(Keys.A) ||
+                    ButtonState.Pressed == currentGamePadState.DPad.Left);
+        }
+
+        private bool IsRightKey()
+        {
+            return (currentKeyboardState.IsKeyDown(Keys.Right) ||
+                    currentKeyboardState.IsKeyDown(Keys.D) ||
+                    ButtonState.Pressed == currentGamePadState.DPad.Right);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            // Start drawing
+            spriteBatch.Begin();
+
+            // Draw the Player
+            player.Draw(spriteBatch);
+
+            // Stop drawing
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Win8ShooterGame/Player.cs
@@ -0,0 +1,73 @@
+﻿// using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Win8ShooterGame;
+
+namespace Shooter
+{
+    class Player
+    {
+        // Static Texture representing the player
+        //public Texture2D PlayerTexture;
+
+        // Animation representing the player
+        public Animation PlayerAnimation;
+
+        // Position of the Player relative to the upper left side of the screen
+        public Vector2 Position;
+
+        // State of the player
+        public bool Active;
+
+        // Amount of hit points that player has
+        public int Health;
+
+        // Get the width of the player ship
+        public int Width
+        {
+            //get { return PlayerTexture.Width; }       // Static Player
+            get { return PlayerAnimation.FrameWidth; }  // Animated Player
+        }
+
+        // Get the height of the player ship
+        public int Height
+        {
+            //get { return PlayerTexture.Height; }          // Static Player
+            get { return PlayerAnimation.FrameHeight; }     // Animated Player
+
+        }
+
+        //public void Initialize(Texture2D texture, Vector2 position)   // Static Player
+        public void Initialize(Animation animation, Vector2 position)   // Animated Player
+        {
+            //PlayerTexture = texture;      // Static Player
+            PlayerAnimation = animation;    // Animated Player
+
+            // Set the starting position of the player around the middle of
+            // the screen and to the back
+            Position = position;
+
+            // Set the player to be active
+            Active = true;
+
+            // Set the player health
+            Health = 100;
+        }
+
+        // Update the player animation
+        //public void Update()                  // Static Player
+        public void Update(GameTime gameTime)   // Animated Player
+        {
+            // Animated Player
+            PlayerAnimation.Position = Position;
+            PlayerAnimation.Update(gameTime);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            PlayerAnimation.Draw(spriteBatch);                                  // Animated Player
+            //spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f,  // Static Player
+                //Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
